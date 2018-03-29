78be6b2cb938dcb91bd97814219a611a129d4d59
ChengFeng AO <414356022@qq.com>
2018-03-29 11:15:25 +0800

    section3 PracticeB.java

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -6,7 +6,17 @@ import java.util.Map;
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        for(String element :object.get("value"))
+        {
+            collectionA.forEach((k,v) ->
+            {
+                if(k==element)
+                {
+                    collectionA.put(k,v-v/3);
+                }
+            });
+        }
 
-        return null;
+        return collectionA;
     }
 }
